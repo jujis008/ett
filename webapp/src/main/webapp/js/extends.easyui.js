40ab2939678aabaa56a072e8185ec2a3b03cb47c
smartken0824 <smartken0824@gmail.com>
2011-05-31 09:39:23 +0000

    继续移植考试预约功能

--- webapp/src/main/webapp/js/extends.easyui.js
+++ webapp/src/main/webapp/js/extends.easyui.js
@@ -128,3 +128,10 @@ $.extend($.fn.validatebox.defaults.rules, {
  $.fn.form.defaults.onLoadError=function(){
 	 $.messager.alert("Error","Can't access to the remote site.","error");
  }
+ 
+  $.fn.datagrid.defaults.fit=true;
+ $.fn.datagrid.defaults.pagination=true;
+ $.fn.datagrid.defaults.nowrap=true;
+ $.fn.datagrid.defaults.idField="Id";
+ $.fn.datagrid.defaults.rownumbers=true;
+ $.fn.datagrid.defaults.pageSize=20;
