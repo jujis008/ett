6980543ab5518a828d3b01b2b411afc0359d485b
smartken0824 <smartken0824@gmail.com>
2011-07-07 02:55:42 +0000

    修正sql错误，驾校统计未完成

--- dao/src/main/java/com/ett/drv/mapper/TestBytes.java
+++ dao/src/main/java/com/ett/drv/mapper/TestBytes.java
@@ -2,6 +2,8 @@ package com.ett.drv.mapper;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -10,6 +12,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.ett.drv.mapper.self.ITransactResourceMapper;
+import com.ett.drv.mapper.statis.IStatisCoachCarMapper;
+import com.ett.drv.mapper.statis.IStatisDriverMapper;
+import com.ett.drv.mapper.statis.IStatisKmMapper;
+import com.ett.drv.mapper.statis.IStatisSchoolMapper;
 import com.ett.drv.model.self.TransactResourceModel;
 
 public class TestBytes {
@@ -26,16 +32,37 @@ public class TestBytes {
 			reader = Resources.getResourceAsReader(resource);
 			sf=new SqlSessionFactoryBuilder().build(reader);
 			se=sf.openSession();
-			ITransactResourceMapper<TransactResourceModel> trm=se.getMapper(ITransactResourceMapper.class);
-			List<TransactResourceModel> listTr=trm.selectAll();
-			TransactResourceModel tr=(TransactResourceModel)trm.selectEqPk("120110425037090");
-			byte[] bs;
-			for(TransactResourceModel tem: listTr){
-				bs= tem.getAgentIdBackImg();
-				System.err.println(bs.length);
-			}
+//			ITransactResourceMapper<TransactResourceModel> trm=se.getMapper(ITransactResourceMapper.class);
+//			List<TransactResourceModel> listTr=trm.selectAll();
+//			TransactResourceModel tr=(TransactResourceModel)trm.selectEqPk("120110425037090");
+//			byte[] bs;
+//			for(TransactResourceModel tem: listTr){
+//				bs= tem.getAgentIdBackImg();
+//				System.err.println(bs.length);
+//			}
+//			
+//			System.err.println(tr.getAgentIdFrontImg().length);
+			Calendar cal=Calendar.getInstance();
+			Date startDate=cal.getTime();
+			cal.add(Calendar.DATE, 30);
+			Date endDate=cal.getTime();
+			String glbm="454364";
+			String fzjg="xdfad";
+			String idCard="440103198408241530";
+			String hphm="粤A32423";
+			int gtTimes=10;
+			IStatisCoachCarMapper statisCoachCarMapper=se.getMapper(IStatisCoachCarMapper.class);
+			IStatisDriverMapper statisDriverMapper=se.getMapper(IStatisDriverMapper.class);
+			IStatisKmMapper statisKmMapper=se.getMapper(IStatisKmMapper.class);
+			IStatisSchoolMapper statisSchoolMapper=se.getMapper(IStatisSchoolMapper.class);
 			
-			System.err.println(tr.getAgentIdFrontImg().length);
+			statisCoachCarMapper.selectViewCoachCar(fzjg, idCard, hphm);
+			statisDriverMapper.selectViewAddreesRepeatGt(fzjg, startDate, endDate, gtTimes);
+			statisDriverMapper.selectViewYdKsqkGt(fzjg, startDate, endDate, gtTimes);
+			statisDriverMapper.selectViewYdsl(fzjg, glbm, startDate, endDate);
+			statisDriverMapper.selectViewZzzRepeatGt(fzjg, startDate, endDate, gtTimes);
+			statisKmMapper.selectViewKm3KscsGt(fzjg, startDate, endDate, gtTimes);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- model/src/main/java/com/ett/drv/biz/IStatisBiz.java
+++ model/src/main/java/com/ett/drv/biz/IStatisBiz.java
@@ -12,19 +12,29 @@ import com.ett.drv.view.StatisSchoolView;
 
 public interface IStatisBiz {
 
-	
+	//教练车查询
 	public List<StatisCoachCarView> getViewCoachCar(String fzjg ,String idCard,String hphm);
 	
+	
+	//联系地址雷同30个以上
 	public List<StatisDriverView> selectViewAddreesRepeatGt30(String fzjg,Date startDate,Date endDate);
 	
+	//异地数量统计
 	public List<StatisDriverView> selectViewYdsl(String fzjg,String glbm,Date startDate,Date endDate);
 	
+	
+	//异地驾驶人考试情况超过100的
 	public List<StatisDriverView> selectViewYdKsqkGt100(String fzjg,Date startDate,Date endDate);
 	
+	
+	//暂住证号码雷同超过3个
 	public List<StatisDriverView> selectViewZzzRepeatGt3(String fzjg,Date startDate,Date endDate);
 	
+	
+	//考试员科目三一天考试次数超过50
 	public List<StatisKmView> selectViewKm3KscsGt50(String fzjg,Date startDate,Date endDate);
 	
+	//驾校状况统计(未能使用)
 	public List<StatisSchoolView> selectViewSchool(String glbm);
 
 
