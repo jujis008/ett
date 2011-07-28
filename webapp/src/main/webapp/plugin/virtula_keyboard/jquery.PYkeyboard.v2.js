ccef86179c8abb5d493720c06918898dac4eac29
smartken0824 <smartken0824@gmail.com>
2011-07-28 09:20:38 +0000

    按照必武的要求改进拼音键盘，已完成，测试页面放在根目录下的test-pykeyboard.v2.html；准备为项目引入数据字典缓存的功能

--- webapp/src/main/webapp/plugin/virtula_keyboard/jquery.PYkeyboard.v2.js
+++ webapp/src/main/webapp/plugin/virtula_keyboard/jquery.PYkeyboard.v2.js
@@ -1,10 +1,11 @@
 
 
 var KeyboardPinyin=function(opts){
-this.inputObj=opts["inputObj"];
-this.divListArea=opts["divListArea"];
-this.divCode=opts["divCode"];
-this.divSelectWord=opts["divSelectWord"];
+this.target=opts["target"];
+this.setTarget(this.target);
+this.divListArea;
+this.divCode;
+this.divSelectWord;
 this.code_field = "";
 this.candidates = "";
 this.code_len = 12;
@@ -52,7 +53,9 @@ this.fullSharp=true;
 this.sign="all";  
 this.strokes="all"; 
 
-}
+this.mode="EN";   //EN,en,py 
+this.code="";
+};
 
 /*
 KeyboardPinyin.fuhao=[
@@ -86,7 +89,7 @@ KeyboardPinyin.prototype.search_code_table=function(str){
 		else low = mid+1;
 	}
 	return(start);
-}
+};
 
 
 KeyboardPinyin.prototype.create_word_list=function(start, index, str){
@@ -94,19 +97,32 @@ KeyboardPinyin.prototype.create_word_list=function(start, index, str){
 	var cnt = 1;
 	var same_code_words =this.code_table[start].replace(/[a-z';]+/, '').split(',');
 	this.candidates = "";
-	var wordSelects=$("<div></div>");
+	var btnPre=$("<button type=\"button\" class=\"fun\" value=\"pre\" >&lt;</button>");
+	var btnNext=$("<button type=\"button\" class=\"fun\" value=\"next\" >&gt;</button>");
+	var keyboard=this;
+	btnPre.click(function(){
+	        var val=$(this).val();
+			var cls=$(this).attr("class"); 
+			keyboard.on_click(val,cls)}
+	);
+	btnNext.click(function(){
+	        var val=$(this).val();
+			var cls=$(this).attr("class"); 
+			keyboard.on_click(val,cls)}
+	);
+	this.divSelectWord.empty();
 	while (cnt <= 10) {
 		this.candidates += (cnt % 10) + '.' + same_code_words[index] + ' ';
 		//增加选择按钮
 		//var btnSelect="<button type='button' value='"+same_code_words[index]+"'>"
 		//                +same_code_words[index]+"</button>";
-		var btnSelect=$("<button type='button'></button>");
+		var btnSelect=$("<button type='button' class=\"selectWord\"></button>");
 		btnSelect
 		.css("width","auto")
 		.val(same_code_words[index])
 		.html(same_code_words[index])
 		;
-		var target=this.inputObj;
+		var target=this.target;
 		var pykeyboard=this;
 		btnSelect.click(function(){
 		   var val= $(this).val();
@@ -116,7 +132,7 @@ KeyboardPinyin.prototype.create_word_list=function(start, index, str){
 		   pykeyboard.clear_all();
 		});
 		
-		wordSelects.append(btnSelect);
+		this.divSelectWord.append(btnSelect);
 		this.word_list[cnt-1] = same_code_words[index];
 		++index;
 		if (index >= same_code_words.length) {
@@ -133,20 +149,23 @@ KeyboardPinyin.prototype.create_word_list=function(start, index, str){
 	if (start > 0) {
 		if (this.start_stack.length > 1) {
 			this.candidates += '<PgUp  PgDn>';
+			this.divSelectWord.prepend(btnPre).append(btnNext);
 		} else {
 			this.candidates += ' PgDn>';
+			this.divSelectWord.append(btnNext);
 		}
 	} else if (this.start_stack.length > 1) {
 		//for (i=cnt+1; i<=10; i++) document.form1.list_area.value += '\n';
 		this.candidates += '<PgUp';
+		this.divSelectWord.prepend(btnPre);
 	} else {
 		this.candidates += '';//FIXME';
 	}
 	this.start_mem = start;
 	this.index_mem = index;
-	$(this.divListArea).html(this.candidates + "　"); 
-	$(this.divSelectWord).html(wordSelects);
-}
+	//$(this.divListArea).html(this.candidates + "　"); 
+	//this.divSelectWord.html(wordSelects);
+};
 
 
 KeyboardPinyin.prototype.on_code_change=function(str){
@@ -162,20 +181,20 @@ KeyboardPinyin.prototype.on_code_change=function(str){
 		this.index_stack.push(0);
 		if (start >= 0) this.create_word_list(start, 0, str);
 	}
-	$(this.divCode).html(str);
-    $(this.divListArea).html(this.candidates + "　");
+	this.divCode.html(str);
+    //$(this.divListArea).html(this.candidates + "　");
     if(this.candidates==""){
-    	$(this.divSelectWord).empty();
+    	this.divSelectWord.empty();
     }
-}
+};
 
 KeyboardPinyin.prototype.insert_char=function(str){
 	if (str == "") return;
 	if (this.diji) {
-		if (/==/.test(this.inputObj.value)) {
-			this.inputObj.value = this.inputObj.value.replace(/==/, str+"==");	
+		if (/==/.test(this.target.value)) {
+			this.target.value = this.target.value.replace(/==/, str+"==");	
 		} else {
-			this.inputObj.value += str;
+			this.target.value += str;
 		}
 	} else {
 		switch (this.browser) {
@@ -185,7 +204,7 @@ KeyboardPinyin.prototype.insert_char=function(str){
 				r.select();
 				break;
 			case 'NS':
-				var obj = this.inputObj;
+				var obj = this.target;
 				var selectionStart = obj.selectionStart;
 				var selectionEnd = obj.selectionEnd;
 				var oldScrollTop = obj.scrollTop;
@@ -203,10 +222,10 @@ KeyboardPinyin.prototype.insert_char=function(str){
 				}
 				break;
 			default:
-				this.inputObj.value += str;
+				this.target.value += str;
 		}
 	}
-} 
+} ;
 
 KeyboardPinyin.prototype.key_down=function(e){
 	var key = e.which ? e.which : e.keyCode;
@@ -345,10 +364,10 @@ KeyboardPinyin.prototype.key_down=function(e){
 				if (this.lang=="ch") {
 					this.insert_char(this.word_list[(9+parseInt(key_char))%10]);
 					this.code_field = "";
-					$(this.divCode).empty();
+					this.divCode.empty();
 					this.candidates = "";
-					$(this.divListArea).empty();
-					$(this.divSelectWord).empty();
+					//$(this.divListArea).empty();
+					this.divSelectWord.empty();
 					this.cancel_key_event = true;
 					return false;
 				}
@@ -428,7 +447,7 @@ KeyboardPinyin.prototype.key_down=function(e){
 	this.right_arrow = (key == 39)? true : false;
 	
 	return(true);
-}
+};
 
 KeyboardPinyin.prototype.key_up=function(e){
 	var key = e.which ? e.which : e.keyCode;
@@ -445,7 +464,7 @@ KeyboardPinyin.prototype.key_up=function(e){
 		}
 	}
 	return true;
-}
+};
 
 
 
@@ -501,29 +520,248 @@ KeyboardPinyin.prototype.key_press=function(e){
 	}
 
 	return true;
-}
+};
 
 KeyboardPinyin.prototype.highlight_copy=function(){
 	if (this.browser == 'IE') {
-		str_len = this.inputObj.value.length;
+		str_len = this.target.value.length;
 		// document.form1.edit_area.value += '\n------------------\n以上文字借助www.JustInput.com在线中文输入法输入';
-		var range = this.inputObj.createTextRange();
+		var range = this.target.createTextRange();
 		range.execCommand("Copy");
-		this.inputObj.value = this.inputObj.value.substr(0,str_len);
-	} else if (this.inputObj.value.indexOf('JustInput.com') == -1) {
+		this.target.value = this.target.value.substr(0,str_len);
+	} else if (this.target.value.indexOf('JustInput.com') == -1) {
 		// document.form1.edit_area.value += '\n------------------\n以上文字借助www.JustInput.com在线中文输入法输入';
 	}
-	this.inputObj.select();
-}
+	this.target.select();
+};
 
 KeyboardPinyin.prototype.clear_all=function(){
 	this.code_field = "";
-	$(this.divCode).empty();
+	this.code="";
+	this.divCode.empty();
 	this.candidates = "";
-	$(this.divListArea).empty();
-	$(this.divSelectWord).empty();
-}
+	//$(this.divListArea).empty();
+	this.divSelectWord.empty();
+};
 
+KeyboardPinyin.prototype.loadKeyboard=function(regexp){
+	if(!$(regexp)) return false;
+	
+	var btnA=$("<button type=\"button\" class=\"letter\" value=\"A\" >A</button>");
+	var btnB=$("<button type=\"button\" class=\"letter\" value=\"B\" >B</button>");
+	var btnC=$("<button type=\"button\" class=\"letter\" value=\"C\" >C</button>");
+	var btnD=$("<button type=\"button\" class=\"letter\" value=\"D\" >D</button>");
+	var btnE=$("<button type=\"button\" class=\"letter\" value=\"E\" >E</button>");
+	var btnF=$("<button type=\"button\" class=\"letter\" value=\"F\" >F</button>");
+	var btnG=$("<button type=\"button\" class=\"letter\" value=\"G\" >G</button>");
+	var btnH=$("<button type=\"button\" class=\"letter\" value=\"H\" >H</button>");
+	var btnI=$("<button type=\"button\" class=\"letter\" value=\"I\" >I</button>");
+	var btnJ=$("<button type=\"button\" class=\"letter\" value=\"J\" >J</button>");
+	var btnK=$("<button type=\"button\" class=\"letter\" value=\"K\" >K</button>");
+	var btnL=$("<button type=\"button\" class=\"letter\" value=\"L\" >L</button>");
+	var btnM=$("<button type=\"button\" class=\"letter\" value=\"M\" >M</button>");
+	var btnN=$("<button type=\"button\" class=\"letter\" value=\"N\" >N</button>");
+	var btnO=$("<button type=\"button\" class=\"letter\" value=\"O\" >O</button>");
+	var btnP=$("<button type=\"button\" class=\"letter\" value=\"P\" >P</button>");
+	var btnQ=$("<button type=\"button\" class=\"letter\" value=\"Q\" >Q</button>");
+	var btnR=$("<button type=\"button\" class=\"letter\" value=\"R\" >R</button>");
+	var btnS=$("<button type=\"button\" class=\"letter\" value=\"S\" >S</button>");
+	var btnT=$("<button type=\"button\" class=\"letter\" value=\"T\" >T</button>");
+	var btnU=$("<button type=\"button\" class=\"letter\" value=\"U\" >U</button>");
+	var btnV=$("<button type=\"button\" class=\"letter\" value=\"V\" >V</button>");
+	var btnW=$("<button type=\"button\" class=\"letter\" value=\"W\" >W</button>");
+	var btnX=$("<button type=\"button\" class=\"letter\" value=\"X\" >X</button>");
+	var btnY=$("<button type=\"button\" class=\"letter\" value=\"Y\" >Y</button>");
+	var btnZ=$("<button type=\"button\" class=\"letter\" value=\"Z\" >Z</button>");
+	var btn0=$("<button type=\"button\" class=\"num\"  value=\"0\" >0</button>");
+	var btn1=$("<button type=\"button\" class=\"num\" value=\"1\" >1</button>");
+	var btn2=$("<button type=\"button\" class=\"num\" value=\"2\" >2</button>");
+	var btn3=$("<button type=\"button\" class=\"num\" value=\"3\" >3</button>");
+	var btn4=$("<button type=\"button\" class=\"num\" value=\"4\" >4</button>");
+	var btn5=$("<button type=\"button\" class=\"num\" value=\"5\" >5</button>");
+    var btn6=$("<button type=\"button\" class=\"num\" value=\"6\" >6</button>");
+	var btn7=$("<button type=\"button\" class=\"num\" value=\"7\" >7</button>");
+	var btn8=$("<button type=\"button\" class=\"num\" value=\"8\" >8</button>");
+    var btn9=$("<button type=\"button\" class=\"num\" value=\"9\" >9</button>");
+	var btn0=$("<button type=\"button\" class=\"num\" value=\"0\" >0</button>");
+	var btnEN=$("<button type=\"button\" class=\"mode clicked\" value=\"EN\" >EN</button>");
+	var btnEn=$("<button type=\"button\" class=\"mode\" value=\"en\" >en</button>");
+	var btnPY=$("<button type=\"button\" class=\"mode\" value=\"PY\" >拼音</button>");
+	var btnBck=$("<button type=\"button\" class=\"fun\" value=\"bck\" >退格</button>");
+	var btnClr=$("<button type=\"button\" class=\"fun\" value=\"clr\" >清空</button>");
+    var btnSpc=$("<button type=\"button\" class=\"sign\" value=\"spc\" >空格</button>");
+	//var divTop=$("<div class=\"top\"></div>");
+	var tableTop=$("<table class=\"top\" border=1></table>");
+	this.divCode=$("<td class=\"code\"></td>");
+	this.divSelectWord=$("<td class=\"selectWords\"></td>");
+	var tr=$("<tr></tr>");
+	tr.append(this.divCode).append(this.divSelectWord);
+	tableTop.append(tr);
+	
+	var divKeyboard=$("<div></div>");
+	divKeyboard
+	.append(tableTop)
+	.append(btn1)
+	.append(btn2)
+	.append(btn3)
+	.append(btn4)
+	.append(btn5)
+	.append(btn6)
+	.append(btn7)
+	.append(btn8)
+	.append(btn9)
+	.append(btn0)
+	.append(btnBck).append("<br/>")
+	.append(btnQ)
+	.append(btnW)
+	.append(btnE)
+	.append(btnR)
+	.append(btnT)
+	.append(btnY)
+	.append(btnU)
+	.append(btnI)
+	.append(btnO)
+	.append(btnP)
+	.append(btnClr).append("<br/>")
+	.append(btnA)
+	.append(btnS)
+	.append(btnD)
+	.append(btnF)
+	.append(btnG)
+	.append(btnH)
+	.append(btnJ)
+	.append(btnK)
+	.append(btnL).append("<br/>")
+	.append(btnZ)
+	.append(btnX)
+	.append(btnC)
+	.append(btnV)
+	.append(btnB)
+	.append(btnN)
+	.append(btnM)
+	.append(btnSpc)
+	.append(btnEN)
+	.append(btnEn)
+	.append(btnPY)
+	
+	;
+	var keyborad=this;
+	$(regexp).html(divKeyboard);
+    divKeyboard.find("button").each(function(index){
+		$(this).click(function(){
+			var val=$(this).val();
+			var cls=$(this).attr("class");
+			keyborad.on_click(val,cls);
+		});
+	});
+    divKeyboard.find("button.mode").each(function(index){
+		$(this).click(function(){
+              divKeyboard.find("button.mode").removeClass("clicked");
+              $(this).addClass("clicked");
+		});
+	});
+};
+
+
+KeyboardPinyin.prototype.on_click=function(val,cls){
+	if(!val)return ;
+	switch(cls){
+	 case "num" :{
+		 if(this.mode!="EN" && this.mode!="en" ) {break;}
+		 var tval=$(this.target).val();
+		 var newVal=tval+val;
+		 $(this.target).val(newVal);
+		 break;
+	 }
+     case "letter" :{
+		 var tval=$(this.target).val();
+		 if(this.mode=="en"){
+			 val=val.toString().toLowerCase();
+		 }else if(this.mode=="EN"){
+			 val=val.toString().toUpperCase();
+		 }else if(this.mode="py"){
+			 //var code=$.trim(this.divCode.html());
+			 this.code+=val.toString().toLowerCase();
+			 this.on_code_change(this.code);
+			 break;
+		 }
+		 var newVal=tval+val;
+		 $(this.target).val(newVal);
+		 break;
+	 }
+     
+     case "sign" :{
+	     if(val=="spc"){
+		   var tval=$(this.target).val();
+		   var newVal=tval+" ";
+		 $(this.target).val(newVal);
+		 }
+    	 break;
+     }
+     
+     case "mode" :{
+    	 this.mode=val;
+    	 break;
+     }
+     case "fun" :{
+    	 if(val=="bck"){
+    		 if(this.mode=="py"){
+			   if(this.code.length>0){
+				  this.code=this.code.substr(0,this.code.length-1);
+				  this.on_code_change(this.code);
+			   }else{
+			     var tval=$(this.target).val();
+    			 tval=tval.substr(0,tval.length-1);
+    			 $(this.target).val(tval);
+			   }
+    		 }else if(this.mode=="EN" || this.mode=="en"){
+    			 var tval=$(this.target).val();
+    			 tval=tval.substr(0,tval.length-1);
+    			 $(this.target).val(tval);
+    		 }
+    	 }else if(val=="clr"){
+		     if(this.mode=="py"){
+			    if(this.code.length>0){
+				  this.clear_all();
+				}else{
+				  $(this.target).val("");
+				}		    
+			 }else if(this.mode=="EN" || this.mode=="en"){
+			    $(this.target).val("");
+			 }
+    	 }else if(val=="pre"){
+		 	if (this.code.length>0) {
+				if(this.start_stack.length > 1) {
+					this.start_stack.pop();	this.index_stack.pop();
+					this.create_word_list(this.start_stack[this.start_stack.length-1], this.index_stack[this.index_stack.length-1], this.code_field);
+				}
+				this.cancel_key_event = true;
+			}
+		 }else if(val=="next"){
+		   	if (this.code.length>0) {
+				if (this.start_mem != -1) {
+					this.start_stack.push(this.start_mem);
+					this.index_stack.push(this.index_mem);
+					for(i=0; i<=9; i++) {
+						this.word_list[i] = "";
+					}
+					this.create_word_list(this.start_mem, this.index_mem, this.code_field);
+				}
+				this.cancel_key_event = true;
+			}
+		 }
+		 break;
+     }
+	 default:{break;}	 
+	}
+	$(this.target).focus();
+};
+
+KeyboardPinyin.prototype.setTarget=function(target){
+	$(this.target).removeClass("focus");
+	this.target=target;
+	$(this.target).addClass("focus");
+};
 
 
 
